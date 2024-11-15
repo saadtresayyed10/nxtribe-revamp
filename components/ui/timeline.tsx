8c89739a9d0c2d0f5c04148853001f3f724d04c8
Saad Sayyed <saadsyed950@gmail.com>
2024-11-15 11:10:07 +0530

    Mobile TimeLine done

--- components/ui/timeline.tsx
+++ components/ui/timeline.tsx
@@ -38,7 +38,7 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
       ref={containerRef}
     >
       <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <h1 className="dark:text-white font-unbounded lg:text-4xl font-semibold text-center capitalize">
+        <h1 className="dark:text-white font-unbounded lg:text-4xl text-2xl font-semibold text-center capitalize">
           How it works?
         </h1>
       </div>
