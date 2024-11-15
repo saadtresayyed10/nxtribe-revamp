d864880df519f9e12e7abb7d08056e343621d287
Saad Sayyed <saadsyed950@gmail.com>
2024-11-15 11:08:16 +0530

    Mobile Services done

--- app/_components/HomePage/Services.tsx
+++ app/_components/HomePage/Services.tsx
@@ -61,7 +61,7 @@ export function Services() {
     },
   ];
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  relative z-10 py-10 max-w-7xl mx-auto">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 relative z-10 lg:gap-0 gap-4 py-10 lg:p-0 p-10 max-w-7xl mx-auto">
       {features.map((feature, index) => (
         <Feature key={feature.title} {...feature} index={index} />
       ))}
